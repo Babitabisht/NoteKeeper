b828c0a634f36318be42308c202992dacff5706e
babitabisht <babitabisht2018@gmail.com>
2019-11-02 16:52:05 +0530

    model class added

--- lib/models/note.dart
+++ lib/models/note.dart
@@ -35,4 +35,20 @@ class Note {
   set date(String new_date) {
     this._date = new_date;
   }
+
+  Map<String, dynamic> toMap() {
+    var map = Map<String, dynamic>();
+    if (_id != null) {
+      map["id"] = _id;
+    }
+
+    map["title"] = _title;
+    map["description"] = _description;
+    map["date"] = _date;
+    map["priorty"] = _priorty;
+  }
+
+
+
+  
 }
